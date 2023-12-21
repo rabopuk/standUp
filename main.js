b912909b41d4c407fbbd3a04501cf4ebf3e9a170
rabopuk <rabopuk@gmail.com>
2023-12-21 18:06:09 +0400

    add: notification and input validate

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+import { Notification } from './scripts/notification.js';
 import './style.css';
 import TomSelect from 'tom-select';
+import JustValidate from 'just-validate';
+import Inputmask from 'inputmask';
 
 const MAX_COMEDIANS = 6;
 const bookingComediansList = document.querySelector('.booking__comedians-list');
+const bookingForm = document.querySelector('.booking__form');
+const notification = Notification.getInstance();
 
 const createComedianBlock = (comedians) => {
   const bookingComedian = document.createElement('li');
@@ -101,6 +106,91 @@ const init = async () => {
   const comedianBlock = createComedianBlock(comedians);
 
   bookingComediansList.append(comedianBlock);
+
+  const validate = new JustValidate(bookingForm, {
+    errorFieldCssClass: 'booking__input_invalid',
+    successFieldCssClass: 'booking__input_valid',
+  });
+
+  const bookingInputFullname = document.querySelector('.booking__input_fullname');
+  const bookingInputPhone = document.querySelector('.booking__input_phone');
+  const bookingInputTicket = document.querySelector('.booking__input_ticket');
+
+  new Inputmask('+7(999)999-99-99').mask(bookingInputPhone);
+  new Inputmask('99999999').mask(bookingInputTicket);
+
+  validate
+    .addField(bookingInputFullname, [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните имя и фамилию',
+      }])
+    .addField(bookingInputPhone, [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните номер телефона',
+      },
+      {
+        validator() {
+          const phone = bookingInputPhone.inputmask.unmaskedvalue();
+          return phone.length === 10 && !!Number(phone);
+        },
+        errorMessage: 'Некорректный номер телефона',
+
+      }])
+    .addField(bookingInputTicket, [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните номер билета',
+      },
+      {
+        validator() {
+          const ticket = bookingInputTicket.inputmask.unmaskedvalue();
+          return ticket.length === 8 && !!Number(ticket);
+        },
+        errorMessage: 'Неверный номер билета',
+      }
+    ]).onFail((fields) => {
+      let errorMessage = '';
+
+      for (const key in fields) {
+        if (!Object.hasOwnProperty.call(fields, key)) {
+          continue;
+        }
+
+        const element = fields[key];
+
+        if (!element.isValid) {
+          errorMessage += `${element.errorMessage}, `;
+        };
+      }
+
+      notification.show(errorMessage.slice(0, -2), false);
+    });
+
+  bookingForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = { booking: [] };
+    const times = new Set();
+
+    new FormData(bookingForm).forEach((value, field) => {
+      if (field === 'booking') {
+        const [comedian, time] = value.split(',');
+
+        if (comedian && time) {
+          data.booking.push({ comedian, time });
+          times.add(time);
+          notification.show('Бронь принята', true);
+        }
+      } else {
+        data[field] = value;
+      }
+
+      if (times.size !== data.booking.length) {
+        notification.show('Выбраны выступления комиков в одинаковое время', false);
+      }
+    });
+  });
 };
 
 init();
---
+++ scripts/notification.js
@@ -0,0 +1,56 @@
+export class Notification {
+  static instance;
+
+  constructor() {
+    if (Notification.instance) {
+      return Notification.instance;
+    }
+
+    this.timeout = 3000;
+    Notification.instance = this;
+  }
+
+  static getInstance() {
+    if (!Notification.instance) {
+      Notification.instance = new Notification();
+    }
+
+    return Notification.instance;
+  }
+
+  show(message, isSuccess) {
+    const notification = this.createNotification(message, isSuccess);
+
+    document.body.append(notification);
+    this.animationOfNotification(notification, true);
+
+    setTimeout(() => {
+      this.animationOfNotification(notification, false).then(() => {
+        notification.remove();
+      });
+    }, this.timeout);
+  }
+
+  createNotification(message, isSuccess) {
+    const notification = document.createElement('div');
+
+    notification.className = `notification ${isSuccess ? 'notification_success' : 'notification_error'}`;
+    notification.textContent = message;
+
+    return notification;
+  }
+
+  animationOfNotification(notification, show) {
+    return new Promise(resolve => {
+      if (show) {
+        requestAnimationFrame(() => {
+          notification.classList.add('notification_show');
+          resolve();
+        });
+      } else {
+        notification.classList.remove('notification_show');
+        setTimeout(resolve, 500);
+      }
+    });
+  }
+};
